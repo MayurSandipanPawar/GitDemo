1cab9aa1414aed77925c29f8dbc77a4922c97039
MayurSandipanPawar <mayurpawar2111@gmail.com>
2022-11-30 15:53:45 +0530

    arch2() is method is added by architect 2.

--- src/gitDemo/GitDemoClass.java
+++ src/gitDemo/GitDemoClass.java
@@ -12,6 +12,9 @@ public class GitDemoClass {
 	public void developBranch() {
 		System.out.println("This code is created in develop branch.");
 	}
+	public void arch2() {
+		System.out.println("This method is developed by architect 2.");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
