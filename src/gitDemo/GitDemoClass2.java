46b01ab4a9a99d7063e6e5baac993792104580ec
mayur <mayurpawar2111@gmail.com>
2022-11-30 12:46:44 +0530

    This is commit from original project

--- src/gitDemo/GitDemoClass2.java
+++ src/gitDemo/GitDemoClass2.java
@@ -1,9 +1,13 @@
 package gitDemo;
 
 public class GitDemoClass2 {
+	
+	public void getData() {
+		System.out.println("Updated from original source.");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hi, How are you?");
+		
 	}
 
 }
